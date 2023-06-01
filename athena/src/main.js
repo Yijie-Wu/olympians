3bf86e8157ef2ce566fcb125dba5966da0382ec1
Yijie-Wu <1694517106@qq.com>
2023-06-01 22:26:08 +0800

    update ui

--- athena/src/main.js
+++ athena/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router).use(ElementPlus).mount('#app')
